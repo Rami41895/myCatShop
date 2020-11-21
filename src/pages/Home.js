f7f4d4f1ae9d54ad6acd770b33f03e771258c1e8
Rami41895 <ramiimar051@gmail.com>
2020-11-21 20:57:53 +0100

    myCatShop

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./pages.css";
+
+export default function Home() {
+  return (
+    <div className="body">
+      <h1>WELCOME TO THE </h1>
+      <h2>CAT SHOP </h2>
+      <img src='/imgs/bengal2.jpg' alt='img'></img>
+    </div>
+  );
+}
